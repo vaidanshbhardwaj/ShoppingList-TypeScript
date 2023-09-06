0efad894e81dd54aa2dd97fe18562e0ec8c16912
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2023-09-06 20:12:38 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ShoppingList from "./components/ShoppingList";
+import Item from "./models/item";
+import "./App.css";
+import ShoppingListForm from "./components/ShoppingListForm";
+import { v4 as getId } from "uuid";
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const addItem = (product: string, quantity: number) => {
+    setItems([...items, { id: getId(), product, quantity }]);
+  };
+  // const items = [
+  // { id: 1, product: "Lemon", quantity: 3 },
+  // { id: 2, product: "Wafers", quantity: 4 },
+  // ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   );
 }
---
+++ src/components/ShoppingListForm.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+
+interface ShoppingListFormProps {
+  onAddItem: (item: string, quantity: number) => void;
+}
+
+export default function ShoppingListForm({
+  onAddItem,
+}: ShoppingListFormProps): JSX.Element {
+  const productInputRef = useRef<HTMLInputElement>(null);
+  const quantityInputRef = userRef<HTMLInputElement>(null);
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const newProduct = productInputRef.current!.value;
+    const quantity = parseInt(quantityInputRef.current!.value);
+    onAddItem(newProduct, quantity);
+    productInputRef.current!.value = "";
+    quantityInputRef.current!.value = 1;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Product Name" ref={productInputRef} />
+      <input type="number" min={0} ref={quantityInputRef} />
+      <button type="submit">Add Item</button>
+    </form>
+  );
+}
